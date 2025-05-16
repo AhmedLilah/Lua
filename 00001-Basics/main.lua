22ab9feddc2c5d33324353d78ab8351b56e95450
Ahmed Lilah <sabrylilah2325667@gmail.com>
2025-05-16 06:34:33 +0300

    Starting the Lua Journey

---
+++ 00001-Basics/main.lua
@@ -0,0 +1,13 @@
+-- function fac(n)
+-- 	if n == 0 then
+-- 		return 1
+-- 	else
+-- 		return n * fac(n - 1)
+-- 	end
+-- end
+--
+-- print("Enter a number: ")
+-- a = io.read("*n")
+-- print(fac(a))
+
+print(4 and 5)
